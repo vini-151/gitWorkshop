d0c561d6e9485f38b3addd6ba067a20352e4899b
Victor Zerefos <anything.codable@gmail.com>
2025-08-13 14:29:56 -0300

    Initial commit

---
+++ GitWorkshop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitWorkshop
+//
+//  Created by Victor Zerefos on 13/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            // Comments
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitWorkshop/GitWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitWorkshopApp.swift
+//  GitWorkshop
+//
+//  Created by Victor Zerefos on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct GitWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
