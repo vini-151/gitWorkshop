0d1b5f301f93b6f30d9f441d78754c62e33e605b
Victor Zerefos <anything.codable@gmail.com>
2025-08-13 15:08:04 -0300

    Added style to text Hello world

--- GitWorkshop/ContentView.swift
+++ GitWorkshop/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(Font.largeTitle)
+                .padding()
         }
         .padding()
     }
